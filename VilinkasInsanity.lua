ee7543e3ad82056da0fdb0f4fbde1b9a50db9298
Vilinka <vilinka42@gmail.com>
2018-12-18 08:59:01 +0100

    Remove old drain equation

--- VilinkasInsanity.lua
+++ VilinkasInsanity.lua
@@ -953,7 +953,6 @@ local function GetVoidformTime(currTime)
 			local ins = player.insanity - (vfCurrStackDrain * vfCurrStackTimeLeft)
 			-- Sum d+(x-1)*f*dm, Sum x=m to n => (-1/2)*(m-n-1)*(2*d+f*dm*(m+n-2))=ins => Solve for n, d=6, f=(0.68)
 			local dm, m = vform.drainMod, vform.drainStacks + 1 -- We already drained current stack
-			--local vfRemStacks = (sqrt(dm*dm*(2*m-3)*(2*m-3)+dm*10*(ins+6*m-9)+225)+dm-15) / (2*dm)
             local vfRemStacks = (10*sqrt(2.89*dm*dm*(3-2*m)*(3-2*m)+dm*(204*m-(306-34*ins))+900)+17*dm-300) / (34*dm)
 			return vfCurrStackTimeLeft + (vfRemStacks - vform.drainStacks)
 		end
